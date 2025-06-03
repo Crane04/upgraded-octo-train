73ccee1b42db9709e310f39155568e4c935b5950
Crane04 <mayowayusuf3004@gmail.com>
2025-06-03 18:47:53 +0100

    hdwkj

--- backend/src/utils/groq.ts
+++ backend/src/utils/groq.ts
@@ -3,6 +3,5 @@ require("dotenv").config();
 
 const GROQ_API_KEY = process.env.GROQ_API_KEY;
 export default new Groq({
-  apiKey:
-    GROQ_API_KEY || "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",
+  apiKey: GROQ_API_KEY,
 });
